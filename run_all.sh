eb581d01f89e532034988393a0c85e2030bb62a3
claireyung <61528379+claireyung@users.noreply.github.com>
2025-09-16 13:54:58 +1000

    Add experiments and notebooks

---
+++ run_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+find icemount* -maxdepth 0 -mindepth 0 -type d  -exec bash -c "cd {}; 
+pwd; 
+mpirun  -host localhost ../../../../build/ocean_only/MOM6 -n 8;
+echo 'finished' " \;
+
+#ln -s ../common/input.nml
+#ln -s ../common/MOM_input
+#ln -s ../common/diag_table
+
+find seamount* -maxdepth 0 -mindepth 0 -type d  -exec bash -c "cd {}; 
+pwd; 
+mpirun  -host localhost ../../../../build/ocean_only/MOM6 -n 8;
+echo 'finished' " \;
+
+find iceshelf* -maxdepth 0 -mindepth 0 -type d  -exec bash -c "cd {}; 
+pwd; 
+mpirun  -host localhost ../../../../build/ocean_only/MOM6 -n 8;
+echo 'finished' " \;
+
